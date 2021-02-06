cf4d0e6279e30aba703204cffd69d88391eea1c0
Marek Arendt <marek.arendt@yougov.com>
2021-02-06 20:59:38 +0100

    Modified test and new dummy functions created.

--- cars/car_verifier.py
+++ cars/car_verifier.py
@@ -1,2 +1,10 @@
 def verify_car(make, model):
     pass
+
+
+def fetch_cars_by_make(make):
+    pass
+
+
+def validate_car_exists(make, model, cars):
+    pass
--- cars/tests/test_car_verifier.py
+++ cars/tests/test_car_verifier.py
@@ -1,6 +1,8 @@
 from unittest import TestCase
 from unittest.mock import patch
 
+from cars.car_verifier import fetch_cars_by_make
+from cars.car_verifier import validate_car_exists
 from cars.car_verifier import verify_car
 
 
@@ -12,10 +14,16 @@ class TestCarVerifier(TestCase):
         with self.assertRaises(Exception):
             verify_car(make=bad_car['make'], model=bad_car['model'])
 
-    @patch('cars.car_verifier.requests.get')
-    def test_car_verifier_calls_request_get_function(self, mock_request):
+    @patch('cars.car_verifier.fetch_cars_by_make')
+    def test_car_verifier_calls_fetch_cars_function(self, mock_fetch):
         good_car = {'make': 'Honda', 'model': 'Accord'}
 
         verify_car(make=good_car['make'], model=good_car['model'])
-        self.assertTrue(mock_request.called)
+        self.assertTrue(mock_fetch.called)
 
+    @patch('cars.car_verifier.validate_car_exists')
+    def test_car_verifier_calls_validate_car_exists_function(self, mock_validate):
+        good_car = {'make': 'Honda', 'model': 'Accord'}
+
+        verify_car(make=good_car['make'], model=good_car['model'])
+        self.assertTrue(mock_validate.called)
