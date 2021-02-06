051fd502fb258325a611537e189095d04522d761
Marek Arendt <marek.arendt@yougov.com>
2021-02-06 13:53:14 +0100

    Created dummy "get-cars" endpoint and view.

--- car_api/urls.py
+++ car_api/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from cars.views import GetCarsView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('cars', GetCarsView.as_view(), name='get-cars')
 ]
