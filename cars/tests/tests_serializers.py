9f8cefffe92c8a6e729cdbd66fcb260cee4dfc49
Marek Arendt <marek.arendt@yougov.com>
2021-02-06 15:04:59 +0100

    Created test for serializer

---
+++ cars/tests/tests_serializers.py
@@ -0,0 +1,17 @@
+from unittest import TestCase
+
+from cars.models import Car
+from cars.serializers import CarSerializer
+
+
+class TestCarSerializer(TestCase):
+    def setUp(self) -> None:
+        self.car = Car.objects.create(make='Reno', model='Laguna')
+
+    def test_serializer_returns_expected_output(self):
+        serializer = CarSerializer(self.car)
+        expected_body = {
+            'make': 'Reno',
+            'model': 'Laguna',
+        }
+        self.assertEqual(expected_body, serializer.data)
