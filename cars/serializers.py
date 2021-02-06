c047cbda77dcc1900a6b2cdf187c8d421ec0387d
Marek Arendt <marek.arendt@yougov.com>
2021-02-06 18:07:59 +0100

    Modified create method to raise error when object already exists

--- cars/serializers.py
+++ cars/serializers.py
@@ -1,3 +1,5 @@
+from django.core.exceptions import ObjectDoesNotExist
+from rest_framework import serializers, status
 from rest_framework.serializers import ModelSerializer
 
 from cars.models import Car
@@ -7,3 +9,16 @@ class CarSerializer(ModelSerializer):
     class Meta:
         model = Car
         fields = ['make', 'model']
+
+    def create(self, validated_data):
+        make = validated_data['make']
+        model = validated_data['model']
+        try:
+            Car.objects.get(make=make, model=model)
+        except ObjectDoesNotExist:
+            Car.objects.create(**validated_data)
+
+        object_exists_error = serializers.ValidationError('object already exists')
+        object_exists_error.status_code = status.HTTP_409_CONFLICT
+        raise object_exists_error
+
