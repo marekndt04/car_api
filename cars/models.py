c504937b9969206d64575e2b8acbb6588110f65c
Marek Arendt <marek.arendt@yougov.com>
2021-02-06 16:19:23 +0100

    Added new fields to Car model.

--- cars/models.py
+++ cars/models.py
@@ -4,3 +4,5 @@ from django.db import models
 class Car(models.Model):
     make = models.CharField(max_length=64)
     model = models.CharField(max_length=64)
+    rate = models.FloatField(blank=True, null=True)
+    votes = models.IntegerField(blank=True, null=True)
