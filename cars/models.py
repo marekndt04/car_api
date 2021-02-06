29cb96a740ca933c2a61935719f59e76a3525ae4
Marek Arendt <marek.arendt@yougov.com>
2021-02-06 14:43:32 +0100

    Changed view to be able to return list of cars

--- cars/models.py
+++ cars/models.py
@@ -4,4 +4,3 @@ from django.db import models
 class Car(models.Model):
     make = models.CharField(max_length=64)
     model = models.CharField(max_length=64)
-
--- cars/views.py
+++ cars/views.py
@@ -1,8 +1,17 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from cars.models import Car
+
 
 class GetCarsView(APIView):
 
     def get(self, request):
-        return Response([])
+        cars = []
+        for car in Car.objects.all():
+            cars.append({
+                'make': car.make,
+                'model': car.model,
+            })
+
+        return Response(cars)
