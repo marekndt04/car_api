ec952359e237f23d2d92c6567f4fa38512ac3b62
Marek Arendt <marek.arendt@yougov.com>
2021-02-06 14:01:18 +0100

    Fixed tests - added reverse_lazy function to get url by name - changed body to data as attribute of response

--- cars/tests.py
+++ cars/tests.py
@@ -1,18 +1,20 @@
 import http
 from django.test import TestCase
+from rest_framework.reverse import reverse_lazy
 from rest_framework.test import APIClient
 
 
 class TestGetCarsView(TestCase):
     def setUp(self) -> None:
         self.client = APIClient()
+        self.url = reverse_lazy('get-cars')
 
     def test_get_cars_return_list_of_http_ok_response(self):
-        response = self.client.get('get-cars')
+        response = self.client.get(self.url)
 
         self.assertEqual(response.status_code, http.HTTPStatus.OK)
 
     def test_get_cars_view_returns_list(self):
-        response = self.client.get('get-cars')
+        response = self.client.get(self.url)
 
-        self.assertEqual(type(response.body), list)
+        self.assertEqual(type(response.data), list)
