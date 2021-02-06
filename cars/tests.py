c3eac2b5f448d305419e1aa50db7acbda318e7ef
Marek Arendt <marek.arendt@yougov.com>
2021-02-06 13:54:14 +0100

    Tests for "get-cars" view created

--- cars/tests.py
+++ cars/tests.py
@@ -1,3 +1,18 @@
+import http
 from django.test import TestCase
+from rest_framework.test import APIClient
 
-# Create your tests here.
+
+class TestGetCarsView(TestCase):
+    def setUp(self) -> None:
+        self.client = APIClient()
+
+    def test_get_cars_return_list_of_http_ok_response(self):
+        response = self.client.get('get-cars')
+
+        self.assertEqual(response.status_code, http.HTTPStatus.OK)
+
+    def test_get_cars_view_returns_list(self):
+        response = self.client.get('get-cars')
+
+        self.assertEqual(type(response.body), list)
