d6f59a8b3e6b2af012ad6083e7dedd358d26ef18
Marek Arendt <marek.arendt@yougov.com>
2021-02-07 08:53:36 +0100

    Changed model fields.

--- cars/models.py
+++ cars/models.py
@@ -4,5 +4,5 @@ from django.db import models
 class Car(models.Model):
     make = models.CharField(max_length=64)
     model = models.CharField(max_length=64)
-    rate = models.FloatField(blank=True, null=True)
-    votes = models.IntegerField(blank=True, null=True)
+    rate = models.FloatField(blank=True, default=0)
+    votes = models.IntegerField(blank=True, default=0)
